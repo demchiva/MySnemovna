7e56ca44e999386953668ff539b43777ba7f1cce
Ivan Demchenko <ivan.demchenko@lmc.eu>
2023-02-28 20:38:29 +0100

    init

---
+++ src/main/java/cz/my/snemovna/SnemovnaApplication.java
@@ -0,0 +1,13 @@
+package cz.my.snemovna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnemovnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnemovnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/my/snemovna/SnemovnaApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.my.snemovna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnemovnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
