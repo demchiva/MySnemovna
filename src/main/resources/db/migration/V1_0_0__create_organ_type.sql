9c1ddfbcf72fdc9277e3e6b434dabf66a22f7c81
ivan.demchenko <ivan.demchenko@almamedia.com>
2023-04-08 10:30:16 +0200

    fix script

--- src/main/resources/db/migration/V1_0_0__create_organ_type.sql
+++ src/main/resources/db/migration/V1_0_0__create_organ_type.sql
@@ -1,10 +1,8 @@
 CREATE TABLE IF NOT EXISTS organ_type (
-    id INTEGER NOT NULL,
+    id INTEGER NOT NULL PRIMARY KEY,
     parent_id INTEGER,
     name_cz VARCHAR,
     name_en VARCHAR,
     common_type INTEGER,
-    priority INTEGER,
-    PRIMARY KEY (id),
-    FOREIGN KEY (parent_id) references organ_type(id)
+    priority INTEGER
 )
