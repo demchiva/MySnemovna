58a659cc7e6d43b84233e0498d8b70e197fbd975
ivan.demchenko <ivan.demchenko@almamedia.com>
2023-04-09 18:33:10 +0200

    fix case sensitive search

--- src/main/java/cz/my/snemovna/jpa/repository/members/ParliamentMemberRepository.java
+++ src/main/java/cz/my/snemovna/jpa/repository/members/ParliamentMemberRepository.java
@@ -18,11 +18,11 @@ public interface ParliamentMemberRepository extends JpaRepository<ParliamentMemb
             "join Organ party on pm.partyId = party.id " +
             "join Organ region on pm.regionId = region.id " +
             "join Organ period on pm.periodId = period.id " +
-            "where person.firstName like :search% " +
-            "or person.lastName like :search% " +
-            "or party.nameCz like :search% " +
-            "or region.nameCz like :search% " +
-            "or str(period.dateFrom) like :search% " +
-            "or str(period.dateTo) like :search%")
+            "where lower(person.firstName) like lower(concat(:search, '%')) " +
+            "or lower(person.lastName) like lower(concat(:search, '%')) " +
+            "or lower(party.nameCz) like lower(concat(:search, '%')) " +
+            "or lower(region.nameCz) like lower(concat(:search, '%')) " +
+            "or str(period.dateFrom) like lower(concat(:search, '%')) " +
+            "or str(period.dateTo) like lower(concat(:search, '%'))")
     Page<ParliamentMember> findBySearch(@Param("search") String search, Pageable page);
 }
