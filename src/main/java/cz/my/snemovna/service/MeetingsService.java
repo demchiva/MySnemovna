05afb2293fa08ba21d6e44783d688523c3174c9b
ivan.demchenko <ivan.demchenko@almamedia.com>
2023-04-11 23:27:09 +0200

    fix logic mistake

--- src/main/java/cz/my/snemovna/service/MeetingsService.java
+++ src/main/java/cz/my/snemovna/service/MeetingsService.java
@@ -149,7 +149,7 @@ public class MeetingsService implements IMeetingsService {
         return new MeetingDetailDto(
                 meetingPoints
                         .stream()
-                        .filter(e -> MEETING_POINT_TYPES_INTERPELLATION_ANSWER.equals(e.getTypeId()))
+                        .filter(e -> !MEETING_POINT_TYPES_INTERPELLATION_ANSWER.equals(e.getTypeId()))
                         .filter(e -> e.getPointNumber() >= MINIMAL_MEETING_POINT_ID)
                         .map(e -> createPointDto(e, states.getOrDefault(e.getStateId(), null)))
                         .toList()
